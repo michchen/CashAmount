576fb8cd0a8bca422c636851ea0e0380c33e1339
Michelle Chen <michchen58@gmail.com>
2018-08-30 09:57:55 -0700

    done

--- CashAmount.js
+++ CashAmount.js
@@ -17,30 +17,18 @@ class CashAmount {
   }
 
   totalInPennies() {
-    // const cash = new CashAmount(10.50);
-    // cash.totalInPennies(); // -> 1050
     return this.amount * 100;
   }
 
   addDoubleAmount(toAdd) {
-    // const cash = new CashAmount(10.50);
-    // cash.addDoubleAmount(29.33);
-    // cash.totalInPennies(); // -> 3983
-
-    let first = this; // amount of cash, aka "this"
-    let second = new CashAmount(toAdd); // amount of second cash value (param)
-
-    // convert both to pennies, add together
-
-    this.amount = this.amount + second.amount;
+    let first = this;
+    if (toAdd !== undefined) {
+      let second = new CashAmount(toAdd);
+      this.amount = (this.totalInPennies() + second.totalInPennies()) / 100;
+    }
   }
 
   quantityOfEachDenomination() {
-    // const cash = new CashAmount(967.93);
-    // cash.quantityOfEachDenomination()
-    // -> { 'hundreds': 9, 'fifties': 1, 'twenties': 0, 'tens': 1, 'fives': 1, 'ones': 2, 'quarters': 3, 'dimes': 1, 'nickels': 1, 'pennies': 3 }
-    //     let c = this.amount;
-
     let billsCoins = [10000, 5000, 2000, 1000, 500, 100, 25, 10, 5, 1];
     let billCoinNames = {
       10000: "hundreds",
@@ -57,36 +45,31 @@ class CashAmount {
     let result = {};
 
     var recurse = function(amount, i) {
-      let num = 0;
-      let curBillOrCoin = billsCoins[i];
+      let numOfAmount = 0;
+      let curBillCoin = billsCoins[i];
 
-      while (amount >= curBillOrCoin) {
-        amount = amount - curBillOrCoin;
-        num++;
+      while (amount >= curBillCoin) {
+        amount = amount - curBillCoin;
+        numOfAmount++;
       }
-      if (amount < curBillOrCoin && i < billsCoins.length) {
-        result[billCoinNames[curBillOrCoin]] = num;
+      if (amount < curBillCoin && i < billsCoins.length) {
+        result[billCoinNames[curBillCoin]] = numOfAmount;
         return recurse(amount, i + 1);
       }
     };
 
     recurse(this.amount * 100, 0);
-    console.log(result);
   }
 
   toDouble() {
-    // const cash = new CashAmount(10.50);
-    // cash.addDoubleAmount(29.33);
-    // cash.toDouble(); // -> 39.83
+    return this.amount;
   }
 
   toDoubleString() {
-    // const cash = new CashAmount(10.50);
-    // cash.addDoubleAmount(29.33);
-    // cash.toDoubleString(); // -> '39.83'
+    return String(this.amount);
   }
 }
 
-var c = new CashAmount(967.93);
-
-c.quantityOfEachDenomination();
+// var c = new CashAmount(967.93);
+//
+// c.quantityOfEachDenomination();
